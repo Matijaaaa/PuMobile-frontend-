15c214e8c2334f6a6ceceee5c2ef7cd0d95eca36
MatijaFabris <75133090+Matijaaaa@users.noreply.github.com>
2024-02-06 16:55:31 +0100

    update: button disabled dok se ne odabere opcija

--- src/views/checkpoint-view.vue
+++ src/views/checkpoint-view.vue
@@ -4,18 +4,55 @@
       <h1 class="text-8xl font-semibold uppercase tekst">PuMobile</h1>
     </div>
     <div class="mt-4 d-flex justify-center">
-      <b-dropdown text="Nearest location" variant="primary">
-        <b-dropdown-item>Option 1</b-dropdown-item>
-        <b-dropdown-item>Option 2</b-dropdown-item>
-        <b-dropdown-item>Option 3</b-dropdown-item>
+      <b-dropdown :text="selectedOptionText" variant="primary">
+        <b-dropdown-item @click="selectOption('Gardini')"
+          >Gardini</b-dropdown-item
+        >
+        <b-dropdown-item @click="selectOption('Forum')">Forum</b-dropdown-item>
+        <b-dropdown-item @click="selectOption('Tržnica')"
+          >Tržnica</b-dropdown-item
+        >
+        <b-dropdown-item @click="selectOption('Bus Station')"
+          >Bus Station</b-dropdown-item
+        >
       </b-dropdown>
+    </div>
+    <div class="d-flex justify-center mt-16">
+      <v-btn
+        depressed
+        color="blue"
+        to="/timep"
+        x-large
+        rounded
+        @click.prevent="saveData"
+        :disabled="!isOptionSelected"
+      >
+        Save Times
+      </v-btn>
     </div>
   </v-container>
 </template>
 
 <script>
 export default {
-  // your component options
+  data() {
+    return {
+      selectedOptionText: "Nearest location",
+    };
+  },
+  methods: {
+    selectOption(option) {
+      this.selectedOptionText = option;
+    },
+    saveData() {
+      // Add your save logic here
+    },
+  },
+  computed: {
+    isOptionSelected() {
+      return this.selectedOptionText !== "Nearest location";
+    },
+  },
 };
 </script>
 
@@ -25,7 +62,7 @@ export default {
   text-align: center;
 }
 
-/* Style the button with a yellow background */
+/* Style the button with a blue background */
 .b-dropdown-toggle {
   background-color: blue;
 }
